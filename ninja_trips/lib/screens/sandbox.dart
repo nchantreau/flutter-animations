7cae189698d5be411cacea2f0320ba565d1e02ca
Nicolas Chantreau <citrouille86@gmail.com>
2021-07-19 16:14:01 +0200

    Upgrade

--- ninja_trips/lib/screens/sandbox.dart
+++ ninja_trips/lib/screens/sandbox.dart
@@ -15,40 +15,58 @@ class _SandboxState extends State<Sandbox> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedContainer(
-        duration: Duration(seconds: 1),
-        margin: EdgeInsets.all(_margin),
-        width: _width,
-        color: _color,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text('animate margin'),
-              onPressed: () => setState(() => _margin = 50),
-            ),
-            RaisedButton(
-              child: Text('animate color'),
-              onPressed: () => setState(() => _color = Colors.purple),
-            ),
-            RaisedButton(
-              child: Text('animate width'),
-              onPressed: () => setState(() => _width = 400),
-            ),
-            RaisedButton(
-              child: Text('animate opacity'),
-              onPressed: () => setState(() => _opacity = 0),
-            ),
-            AnimatedOpacity(
-              duration: Duration(seconds: 2),
-              opacity: _opacity,
-              child: Text(
-                'hide me',
-                style: TextStyle(color: Colors.white)
+      body: SafeArea(
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          margin: EdgeInsets.all(_margin),
+          width: _width,
+          color: _color,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('animate margin'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade300),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () => setState(() => _margin = 50),
               ),
-            ),
-          ],
-        )
+              ElevatedButton(
+                child: Text('animate color'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade300),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () => setState(() => _color = Colors.purple),
+              ),
+              ElevatedButton(
+                child: Text('animate width'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade300),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () => setState(() => _width = 400),
+              ),
+              ElevatedButton(
+                child: Text('animate opacity'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade300),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () => setState(() => _opacity = 0),
+              ),
+              AnimatedOpacity(
+                duration: Duration(seconds: 2),
+                opacity: _opacity,
+                child: Text(
+                  'hide me',
+                  style: TextStyle(color: Colors.white)
+                ),
+              ),
+            ],
+          )
+        ),
       ),
     );
   }
